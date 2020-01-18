28120740c0c3dbe1aaa6a0d3398544697b909641
1nsider <boobah2424@gmail.com>
2020-01-18 01:07:25 -0700

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -178,7 +178,7 @@ tlStuds.TextWrapped = true
 MinimapGUI.Name = "MinimapGUI"
 MinimapGUI.Parent = esp
 MinimapGUI.Enabled = false
-MinimapGUI.ResetOnSpawn = false
+MinimapGUI.ResetOnSpawn = true
 
 Minimap.Name = "Minimap"
 Minimap.Parent = MinimapGUI
